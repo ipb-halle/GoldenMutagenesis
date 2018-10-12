f54b457b27f246cb6f75ba08f42557ccc7eb2a0d
Steffen Neumann <sneumann@ipb-halle.de>
2018-10-12 11:35:28 +0200

    Preliminary use of biocLite, should be changed to BioC manager later

--- binder/install.R
+++ binder/install.R
@@ -4,7 +4,11 @@ install.packages("stringr")
 install.packages("devtools")
 
 ## BioC dependencies
-install.packages("BiocManager")
-BiocManager::install(c("Biostrings", "sangerseqR"))
+
+#install.packages("BiocManager")
+#BiocManager::install(c("Biostrings", "sangerseqR"))
+
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("Biostrings", "sangerseqR"))
 
 devtools::install_github("ipb-halle/GoldenMutagenesis", ref="prerelease-dev")
