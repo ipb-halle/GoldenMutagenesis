e510aadafd31da2e3c7f093e4c0e020476d43cf6
Chris Ulpinnis <culpinins@gmail.com>
2018-10-16 21:39:22 +0200

    Added more tests

--- inst/tests/test-MSD.R
+++ inst/tests/test-MSD.R
@@ -14,4 +14,17 @@ test_that("Known primers are calculated correctly (MSD) for lowercase sequences"
   expect_that(primer_test_lc, is_identical_to(primers_test))
 })
 
+load("../data/MSD_BsaI_result_lv0.RData")
+primer_test_lvl0<-primer_add_level(primers,  prefix="TT", restriction_enzyme="GAAGAC", suffix="AA", vector=c("CTCA", "CTCG"))
+
+test_that("Adding level0 to a set of primers works", {
+  expect_that(primer_test_lvl0, is_identical_to(primers_lvl0))
+})
+
+primer_test_lvl0_lc<-primer_add_level(primers,  prefix=str_to_lower("TT"), restriction_enzyme=str_to_lower("GAAGAC"), suffix=str_to_lower("AA"), vector=str_to_lower(c("CTCA", "CTCG")))
+
+test_that("Adding level0 to a set of primers works for lowercase sequences", {
+  expect_that(primer_test_lvl0, is_identical_to(primers_test_lvl0_lc))
+})
+
 rm(primers)
