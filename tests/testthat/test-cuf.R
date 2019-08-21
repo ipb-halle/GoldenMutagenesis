dfc67ae17f3342bcfc8d69debfd8818f038f3996
culpinnis <culpinnis@gmail.com>
2019-08-21 19:08:47 +0200

    Modified test for cuf

--- tests/testthat/test-cuf.R
+++ tests/testthat/test-cuf.R
@@ -1,4 +1,4 @@
 context("Codon Usage Tables")
 test_that("Codon Usage Tables can be found", {
-expect_that(list_cu_table(),is_identical_to(c( "arabidopsis.csv","e_coli_316407.csv","s_cerevisiae_4932.csv", "Salinibacter_ruber_DSM_13855.csv")))
+expect_that(list_cu_table(),is_equivalent_to(c( "arabidopsis.csv","e_coli_316407.csv","s_cerevisiae_4932.csv", "Salinibacter_ruber_DSM_13855.csv")))
 })
