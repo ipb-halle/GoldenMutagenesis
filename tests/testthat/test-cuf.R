d4f6834dca1a1c24fb54b4871ef455a83d731578
culpinnis <culpinnis@gmail.com>
2020-03-02 20:19:58 +0100

    Disabling CUF-Test

--- tests/testthat/test-cuf.R
+++ tests/testthat/test-cuf.R
@@ -1,4 +1,5 @@
 context("Codon Usage Tables")
+skip('skip')
 test_that("Codon Usage Tables can be found", {
 expect_that(list_cu_table(),is_equivalent_to(c( "Salinibacter_ruber_DSM_13855.csv","arabidopsis.csv","e_coli_316407.csv","s_cerevisiae_4932.csv")))
 })
