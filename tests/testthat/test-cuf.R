c5a88f564e3885ca455c4ff29db1330fb25fd81a
culpinnis <culpinnis@gmail.com>
2020-03-05 15:50:48 +0100

    Repaired cuf test

--- tests/testthat/test-cuf.R
+++ tests/testthat/test-cuf.R
@@ -1,5 +1,7 @@
 context("Codon Usage Tables")
-skip('skip')
 test_that("Codon Usage Tables can be found", {
-expect_that(list_cu_table(),is_equivalent_to(c( "Salinibacter_ruber_DSM_13855.csv","arabidopsis.csv","e_coli_316407.csv","s_cerevisiae_4932.csv")))
+expect_match(list_cu_table(),"Salinibacter_ruber_DSM_13855.csv", fixed=T, all=F)
+expect_match(list_cu_table(),"arabidopsis.csv", fixed=T, all=F)
+expect_match(list_cu_table(),"e_coli_316407.csv", fixed=T, all=F)
+expect_match(list_cu_table(),"s_cerevisiae_4932.csv", fixed=T, all=F)
 })
